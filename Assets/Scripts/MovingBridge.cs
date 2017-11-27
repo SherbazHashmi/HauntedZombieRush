3395c1941e8cbac0090e783143b6e2678285d2f8
Sherbaz <sherbaz.hashmi@gmail.com>
2017-11-27 19:03:29 +1100

    Creeated foreground moving platform, added it as a prefab, also scripted it's movement.

---
+++ Assets/Scripts/MovingBridge.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBridge : MonoBehaviour {
+
+    [SerializeField] float objectSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate((Vector3.left * (objectSpeed * Time.deltaTime)),Space.World);
+	}
+}
